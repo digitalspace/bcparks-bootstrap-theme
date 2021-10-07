4c9f14135a64f4a1dc7a103f4c247b3aafa6231e
Daniel Truong <daniel@dtsoftware.ca>
2021-10-06 18:19:41 -0700

    Moving to Bootstrap 5

---
+++ src/bootstrap-theme.js
@@ -0,0 +1,2 @@
+// You can specify which plugins you need
+import bootstrap from 'bootstrap';
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: __dirname + "/src",
+
+  entry: ['./bootstrap-theme.js', './styles/bootstrap-theme.scss'],
+
+  output: {
+    path: __dirname + "/dist",
+    publicPath: '../',
+    filename: 'js/bootstrap-theme.min.js'
+  },
+  module: {
+
+    rules: [
+      // loader for regular css files
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
+      // scss loader for webpack
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+      // move fonts into local directory
+      {
+        test: /\.(otf|ttf|eot|woff|woff2|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // name: '[path][name].[ext]',
+              debug: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/bootstrap-theme.min.css',
+    }),
+    // We just copy these because the client may have already a dependency or choose a different JS package
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'fonts',
+          to: 'fonts'
+        },
+        {
+          from: 'images',
+          to: 'images'
+        },
+        {
+          from: 'styles',
+          to: 'scss'
+        }
+      ]
+    }),
+    // new CleanWebpackPlugin({
+    //   verbose: true,
+    //   dry: false
+    // }),
+  ],
+};
+

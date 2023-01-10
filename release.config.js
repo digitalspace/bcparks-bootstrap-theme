e5cd0594801fd685d0dac5a7c8b6ce886a5eeea0
Daniel Truong <daniel@digitalspace.ca>
2023-01-10 23:02:46 +0000

    feat(workflows): creating tag action

--- release.config.js
+++ release.config.js
@@ -1,29 +1,25 @@
 module.exports = {
-  branches: [
-    "master",
-    {
-      name: "beta",
-      prerelease: true,
-    },
-  ],
+  branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    "@semantic-release/npm",
+    "@semantic-release/changelog",
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "dist/**"],
-        message:
-          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    // "@semantic-release/release-notes-generator",
+    // [
+    //   "@semantic-release/changelog",
+    //   {
+    //     changelogFile: "CHANGELOG.md",
+    //   },
+    // ],
+    // "@semantic-release/npm",
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["package.json", "CHANGELOG.md", "dist/**"],
+    //     message:
+    //       "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    //   },
+    // ],
+    // "@semantic-release/github",
   ],
 };

2d967e3ab86f097ed32bf877b3cb17aba9403b10
Daniel Truong <daniel@digitalspace.ca>
2023-01-12 19:15:29 +0000

    fix(workflows): added github plugin

--- release.config.js
+++ release.config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     ],
     "@semantic-release/git",
+    "@semantic-release/github",
     // "@semantic-release/release-notes-generator",
     // [
     //   "@semantic-release/changelog",

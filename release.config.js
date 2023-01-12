2a4fbdc20fe1ea48ddd960896612e1514c66653e
Daniel Truong <daniel@digitalspace.ca>
2023-01-12 19:46:07 +0000

    fix(workflows): revert

--- release.config.js
+++ release.config.js
@@ -1,20 +1,32 @@
 module.exports = {
   branches: ["master"],
   plugins: [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/commit-analyzer",
     [
-      "@semantic-release/changelog",
+      "@semantic-release/npm",
       {
-        changelogFile: "CHANGELOG.md",
+        npmPublish: false,
       },
     ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-      },
-    ],
-    "@semantic-release/github",
+    "@semantic-release/git"
+    // "@semantic-release/release-notes-generator",
+    // [
+    //   "@semantic-release/changelog",
+    //   {
+    //     changelogFile: "CHANGELOG.md",
+    //   },
+    // ],
+    // "@semantic-release/npm",
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["package.json", "CHANGELOG.md", "dist/**"],
+    //     message:
+    //       "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    //   },
+    // ],
+    // "@semantic-release/github",
   ],
 };

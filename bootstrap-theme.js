98e5aa2dc9189f2cf62fa14604511b4abe759535
Mark Lisé <mark@digitalspace.ca>
2024-01-23 10:29:13 -0800

    Fixing theme.

--- bootstrap-theme.js
+++ bootstrap-theme.js
@@ -1,1 +1,15 @@
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+window.Modal = bootstrap.Modal;
+
+window.addEventListener('DOMContentLoaded', function () {
+  initialize();
+}, false);
+
+function initialize() {
+  const tipElements = document.querySelectorAll('[data-toggle="tooltip"]');
+  tipElements.forEach(tooltipEl => {
+    const tooltip = new bootstrap.Tooltip(tooltipEl);
+    tooltip.enable();
+    return tooltip;
+  });
+}
